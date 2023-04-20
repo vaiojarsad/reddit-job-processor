74eeac89e00ed0b7cc8c1c69bfd4d0256d90b947
vaiojarsad <pdcvgmh@gmail.com>
2023-04-19 21:26:39 -0300

    Functional commit

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/vaiojarsad/reddit-job-processor
 go 1.19
 
 require (
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/vaiojarsad/job-processor v0.0.1 // indirect
 	github.com/vaiojarsad/reddit-video-downloader v0.0.3 // indirect
 )
--- reddit_video_download_job_processor.go
+++ reddit_video_download_job_processor.go
@@ -2,7 +2,11 @@ package redditjobprocessor
 
 import (
 	"fmt"
+
+	"github.com/mitchellh/mapstructure"
+
 	jobprocessor "github.com/vaiojarsad/job-processor"
+	"github.com/vaiojarsad/reddit-video-downloader/reddit"
 )
 
 var (
@@ -21,7 +25,22 @@ func NewRedditVideoDownloadJobProcessor(ctx jobprocessor.ExecutionContext) (jobp
 	return &RedditVideoDownloadJobProcessor{}, nil
 }
 
+type RedditVideoDownloadJob struct {
+	VideoURL       string
+	OutputFileName string
+	NoAudio        bool
+	NoVideo        bool
+}
+
 func (jp *RedditVideoDownloadJobProcessor) Process(job jobprocessor.Job) error {
-	fmt.Println(job)
+	var parsedJob RedditVideoDownloadJob
+	err := mapstructure.Decode(job, &parsedJob)
+	if err != nil {
+		return fmt.Errorf("error parsing job: %w", err)
+	}
+	err = reddit.DownloadVideo(parsedJob.VideoURL, parsedJob.OutputFileName, parsedJob.NoAudio, parsedJob.NoVideo)
+	if err != nil {
+		return fmt.Errorf("error downloading video: %w", err)
+	}
 	return nil
 }
